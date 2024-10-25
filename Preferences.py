90e2281cfb3ebecec06a3d6720ee54252f93bc55
TinkerBoi <blenderboi2018@gmail.com>
2024-10-25 15:51:44 +0800

    Automatically Disable Conflicting Keymaps, and Include in Preferences

--- Preferences.py
+++ Preferences.py
@@ -2,9 +2,11 @@ import bpy
 from . Operator import *
 import rna_keymap_ui
 
+addon_keymaps = []
+disabled_keymaps = []
 
-def use_maya_mode():
-    pass
+
+
 
 class SP_user_preferences(bpy.types.AddonPreferences):
     bl_idname = __package__
@@ -43,7 +45,10 @@ class SP_user_preferences(bpy.types.AddonPreferences):
   
             col.context_pointer_set("keymap", km)
             rna_keymap_ui.draw_kmi([], kc, addon_keymap[0], addon_keymap[1], col, 0)
-
+        
+        box = layout.box()
+        for disabled_keymap in disabled_keymaps:
+            rna_keymap_ui.draw_kmi([], kc, disabled_keymap[0], disabled_keymap[1], box, 0)
 
         # else:
         #     col.operator(Template_Add_Hotkey.bl_idname, text = "Add hotkey entry")
@@ -53,7 +58,6 @@ class SP_user_preferences(bpy.types.AddonPreferences):
         col.prop(self, "use_shade_smooth", text="Use Shade Smooth")
         col.prop(self, "maya_mode", text="Maya Mode")
 
-addon_keymaps = []
 
 
 
@@ -68,9 +72,11 @@ def add_hotkey():
     user_preferences = bpy.context.preferences
     addon_prefs = user_preferences.addons[__package__].preferences
 
+
+
+
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
-    km = kc.keymaps.new(name="3D View", space_type='VIEW_3D')
 
 
 
@@ -108,6 +114,24 @@ def add_hotkey():
     kmi = km.keymap_items.new("mvo.viewport_operations", type="SEVEN", value="PRESS")
     kmi.properties.mode = 7
     addon_keymaps.append((km, kmi))
+
+
+
+    kc_active = wm.keyconfigs.get(bpy.context.preferences.keymap.active_keyconfig)
+    for km in kc_active.keymaps:
+        for kmi in km.keymap_items:
+            
+            if km.name in ["Object Mode", "Mesh"]:
+                
+                for addon_keymap in addon_keymaps:
+                    match = addon_keymap[1].compare(kmi)
+                    if match:
+                        kmi.active = False
+                        disabled_keymaps.append((km, kmi))
+                        print("disabled keymap - " + kmi.name + " - Key: " + kmi.to_string())
+                        
+ 
+
 
 
 class Template_Add_Hotkey(bpy.types.Operator):
